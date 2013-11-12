1d0a141ab464338e2096338f7ae39b9e4dce3184
Christopher Harris <ccharri@umich.edu>
2013-11-12 00:13:34 -0500

    .gitignore refreshed

--- mcp/src/minecraft/net/minecraft/logging/LogAgentEmptyAnon.java
+++
@@ -1,5 +0,0 @@
-package net.minecraft.logging;
-
-class LogAgentEmptyAnon
-{
-}
--- mcp/src/minecraft/net/minecraft/world/gen/structure/EnumDoor.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.world.gen.structure;
-
-public enum EnumDoor
-{
-    OPENING,
-    WOOD_DOOR,
-    GRATES,
-    IRON_DOOR;
-}
