be2287c4fa1e3ff2549167a118e3c42bc06768c8
Itszuvalex <Itszuvalex@gmail.com>
2014-01-25 04:06:27 -0500

    ZZzzz This was supposed to be easy.

--- mcp/eclipse/Minecraft/common/femtocraft/industry/blocks/VacuumTube.java
+++ mcp/eclipse/Minecraft/common/femtocraft/industry/blocks/VacuumTube.java
@@ -1,10 +1,7 @@
 package femtocraft.industry.blocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import femtocraft.Femtocraft;
-import femtocraft.industry.TileEntity.VacuumTubeTile;
-import femtocraft.proxy.ClientProxyFemtocraft;
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
@@ -15,8 +12,18 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import femtocraft.Femtocraft;
+import femtocraft.api.IInterfaceDevice;
+import femtocraft.industry.TileEntity.VacuumTubeTile;
+import femtocraft.power.TileEntity.FemtopowerCableTile;
+import femtocraft.proxy.ClientProxyFemtocraft;
 
 public class VacuumTube extends BlockContainer {
 	public Icon indicatorIcon;
@@ -162,16 +169,20 @@ public class VacuumTube extends BlockContainer {
 			float par8, float par9) {
 		
 		if(par1World.isRemote) return true;
-		
-		TileEntity tile = par1World.getBlockTileEntity(par2, par3, par4);
-		if(tile != null)
+		ItemStack item = par5EntityPlayer.getCurrentEquippedItem();
+		if(item != null && item.getItem() instanceof IInterfaceDevice)
 		{
-			if(tile instanceof VacuumTubeTile)
+			
+			TileEntity tile = par1World.getBlockTileEntity(par2, par3, par4);
+			if(tile != null)
 			{
-				VacuumTubeTile tube = (VacuumTubeTile)tile;
-				
-				tube.searchForConnection();
-				return true;
+				if(tile instanceof VacuumTubeTile)
+				{
+					VacuumTubeTile tube = (VacuumTubeTile)tile;
+					
+					tube.searchForConnection();
+					return true;
+				}
 			}
 		}
 		return false;
@@ -202,4 +213,111 @@ public class VacuumTube extends BlockContainer {
         endInsetIcon = par1IconRegister.registerIcon(Femtocraft.ID.toLowerCase() + ":" + "VacuumTube_end_inset");
 	}
 
+	@Override
+	public void addCollisionBoxesToList(World par1World, int x, int y,
+			int z, AxisAlignedBB par5AxisAlignedBB, List par6List,
+			Entity par7Entity) {
+		
+		super.addCollisionBoxesToList(par1World, x, y, z, par5AxisAlignedBB,
+				par6List, par7Entity);
+		
+		TileEntity tile = par1World.getBlockTileEntity(x, y, z);
+		if(tile == null) return;
+		if(!(tile instanceof VacuumTubeTile)) return;
+		VacuumTubeTile tube = (VacuumTubeTile)tile;
+		
+		AxisAlignedBB inputBB = boundingBoxForDirection(tube.getInput(), x, y, z);
+		if(par5AxisAlignedBB.intersectsWith(inputBB))
+		{
+			par6List.add(inputBB);
+		}
+		
+		AxisAlignedBB outputBB = boundingBoxForDirection(tube.getOutput(), x, y, z);
+		if(par5AxisAlignedBB.intersectsWith(outputBB))
+		{
+			par6List.add(outputBB);
+		}
+		
+		
+	}
+	
+	private AxisAlignedBB boundingBoxForDirection(ForgeDirection dir, int x, int y, int z)
+	{
+		double minX = getBlockBoundsMinX();
+		double minY = getBlockBoundsMinY();
+		double minZ = getBlockBoundsMinZ();
+		
+		double maxX = getBlockBoundsMaxX();
+		double maxY = getBlockBoundsMaxY();
+		double maxZ = getBlockBoundsMaxZ();
+		
+		switch(dir)
+		{
+		case UP:
+			maxY = 1;
+			break;
+		case DOWN:
+			minY = 0;
+			break;
+		case NORTH:
+			minZ = 0;
+			break;
+		case SOUTH:
+			maxZ = 1;
+			break;
+		case EAST:
+			maxX = 1;
+			break;
+		case WEST:
+			minX = 0;
+			break;
+		default:
+			break;
+		}
+		
+		return AxisAlignedBB.getAABBPool().getAABB((double)x + minX, (double)y + minY, (double)z + minZ, (double)x + maxX, (double)y + maxY, (double)z + maxZ);
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World,
+			int x, int y, int z) {
+		AxisAlignedBB box = AxisAlignedBB.getAABBPool().getAABB((double)x + getBlockBoundsMinX(), (double)y + getBlockBoundsMinY(), (double)z + getBlockBoundsMinZ(), (double)x + getBlockBoundsMaxX(), (double)y + getBlockBoundsMaxY(), (double)z + getBlockBoundsMaxZ());
+		
+		
+		TileEntity tile = par1World.getBlockTileEntity(x, y, z);
+		if(tile == null) return box;
+		if(!(tile instanceof VacuumTubeTile)) return box;
+		VacuumTubeTile tube = (VacuumTubeTile)tile;
+		
+		AxisAlignedBB inputBB = boundingBoxForDirection(tube.getInput(), x, y, z);
+		AxisAlignedBB outputBB = boundingBoxForDirection(tube.getOutput(), x, y, z);
+		
+		return box.func_111270_a(inputBB.func_111270_a(outputBB));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World par1World,
+			int par2, int par3, int par4) {
+		return getCollisionBoundingBoxFromPool(par1World, par2, par3, par4);
+	}
+	
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess par1iBlockAccess,
+			int x, int y, int z) {
+		AxisAlignedBB box = AxisAlignedBB.getAABBPool().getAABB((double)x + 4.f/16.f, (double)y + 4.f/16.f, (double)z + 4.f/16.f, (double)x + 12.f/16.f, (double)y + 12.f/16.f, (double)z + 12.f/16.f);
+		
+		
+		TileEntity tile = par1iBlockAccess.getBlockTileEntity(x, y, z);
+		if(tile == null) return;
+		if(!(tile instanceof VacuumTubeTile)) return;
+		VacuumTubeTile tube = (VacuumTubeTile)tile;
+		
+		AxisAlignedBB inputBB = boundingBoxForDirection(tube.getInput(), x, y, z);
+		AxisAlignedBB outputBB = boundingBoxForDirection(tube.getOutput(), x, y, z);
+		
+		box = box.func_111270_a(inputBB.func_111270_a(outputBB));
+		
+		setBlockBounds((float)box.minX, (float)box.minY, (float)box.minZ, (float)box.maxX, (float)box.maxY, (float)box.maxZ);
+	}
 }
