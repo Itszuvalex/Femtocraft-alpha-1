86fbe38be245115dd7fd7387b706cd63a0019315
helkarakse <nimcuron@gmail.com>
2014-02-13 19:37:39 +0800

    Added tile entities for nano and femto cable

---
+++ mcp/src/minecraft/femtocraft/power/tiles/TileEntityFemtoCable.java
@@ -0,0 +1,23 @@
+package femtocraft.power.tiles;
+
+import femtocraft.managers.research.EnumTechLevel;
+
+public class TileEntityFemtoCable extends TileEntityPowerBase {
+
+	public TileEntityFemtoCable() {
+		setTechLevel(EnumTechLevel.FEMTO);
+	}
+
+	public boolean connectedAcross() {
+		if (numConnections() == 2) {
+			if (connections[0] && connections[1])
+				return true;
+			if (connections[2] && connections[3])
+				return true;
+			if (connections[4] && connections[5])
+				return true;
+		}
+		return false;
+	}
+
+}
