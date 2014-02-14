81f8b45ff7f85a93fc6478d3550005660803684c
helkarakse <nimcuron@gmail.com>
2014-02-15 00:15:18 +0800

    Merged sequential ifs and removed unnecessary inherited methods from nano/femto tile entity

--- mcp/src/minecraft/femtocraft/power/tiles/TileEntityFemtoCable.java
+++ mcp/src/minecraft/femtocraft/power/tiles/TileEntityFemtoCable.java
@@ -3,19 +3,9 @@ package femtocraft.power.tiles;
 import femtocraft.managers.research.EnumTechLevel;
 
 public class TileEntityFemtoCable extends TileEntityMicroCable {
-
 	public TileEntityFemtoCable() {
+		super();
 		setMaxStorage(10000);
 		setTechLevel(EnumTechLevel.FEMTO);
 	}
-
-	public boolean connectedAcross() {
-		if (numConnections() == 2) {
-			if (connections[0] && connections[1]) return true;
-			if (connections[2] && connections[3]) return true;
-			if (connections[4] && connections[5]) return true;
-		}
-		return false;
-	}
-
 }
--- mcp/src/minecraft/femtocraft/power/tiles/TileEntityMicroCable.java
+++ mcp/src/minecraft/femtocraft/power/tiles/TileEntityMicroCable.java
@@ -5,15 +5,17 @@ import femtocraft.managers.research.EnumTechLevel;
 public class TileEntityMicroCable extends TileEntityPowerBase {
 
 	public TileEntityMicroCable() {
+		super();
 		setMaxStorage(250);
 		setTechLevel(EnumTechLevel.MICRO);
 	}
 
 	public boolean connectedAcross() {
 		if (numConnections() == 2) {
-			if (connections[0] && connections[1]) return true;
-			if (connections[2] && connections[3]) return true;
-			if (connections[4] && connections[5]) return true;
+			if (connections[0] && connections[1] || connections[2] && connections[3] || connections[4] &&
+					connections[5]) {
+				return true;
+			}
 		}
 		return false;
 	}
