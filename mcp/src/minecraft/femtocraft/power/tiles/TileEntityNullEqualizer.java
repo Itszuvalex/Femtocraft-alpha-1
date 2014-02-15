cf5c28a35b41d63462a229a4e25cae603b7c398a
helkarakse <nimcuron@gmail.com>
2014-02-16 02:01:00 +0800

    Completed the orbital and null equalizer code

--- mcp/src/minecraft/femtocraft/power/tiles/TileEntityNullEqualizer.java
+++ mcp/src/minecraft/femtocraft/power/tiles/TileEntityNullEqualizer.java
@@ -1,11 +1,17 @@
 package femtocraft.power.tiles;
 
 import femtocraft.managers.research.EnumTechLevel;
+import net.minecraftforge.common.ForgeDirection;
 
 public class TileEntityNullEqualizer extends TileEntityPowerBase {
 	public TileEntityNullEqualizer() {
 		super();
 		setMaxStorage(10000);
 		setTechLevel(EnumTechLevel.FEMTO);
+	}
+
+	@Override
+	public boolean canAcceptPowerOfLevel(EnumTechLevel level, ForgeDirection from) {
+		return (level.tier == EnumTechLevel.NANO.tier || level.tier == EnumTechLevel.FEMTO.tier);
 	}
 }
