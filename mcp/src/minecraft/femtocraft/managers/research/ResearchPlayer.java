bdb8c50f3512fc1c97eb15de93bf12d2baf05ea3
Christopher Harris <ccharri@umich.edu>
2014-02-22 04:08:40 -0500

    Urg.  Bug fix.  However, Debug Step-throughs says PlayerResearch Packets work.

--- mcp/src/minecraft/femtocraft/managers/research/ResearchPlayer.java
+++ mcp/src/minecraft/femtocraft/managers/research/ResearchPlayer.java
@@ -136,11 +136,11 @@ public class ResearchPlayer {
 	// ---------------------------------------------------------
 
 	private void broadcastPacket(Packet packet, String username) {
-//		EntityPlayerMP player = MinecraftServer.getServer()
-//				.getConfigurationManager().getPlayerForUsername(username);
-//		if (player == null)
-//			return;
-//		PacketDispatcher.sendPacketToPlayer(packet, (Player) player);
+		// EntityPlayerMP player = MinecraftServer.getServer()
+		// .getConfigurationManager().getPlayerForUsername(username);
+		// if (player == null)
+		// return;
+		// PacketDispatcher.sendPacketToPlayer(packet, (Player) player);
 	}
 
 	// ---------------------------------------------------------
@@ -174,7 +174,7 @@ public class ResearchPlayer {
 					techname);
 			status.loadFromNBTTagCompound(data);
 
-			techStatus.put(username, status);
+			techStatus.put(techname, status);
 		}
 	}
 }
