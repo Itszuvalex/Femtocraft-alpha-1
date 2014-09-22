a681600191c86f99c13871ac83a1fcaa447a82d6
ccharri <ccharri@umich.edu>
2014-09-21 22:21:40 -0400

    Added Field/Type Targets to @Configurable

--- src/main/java/com/itszuvalex/femtocraft/configuration/Configurable.java
+++ src/main/java/com/itszuvalex/femtocraft/configuration/Configurable.java
@@ -21,13 +21,16 @@
 
 package com.itszuvalex.femtocraft.configuration;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by Christopher Harris (Itszuvalex) on 9/10/14.
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
 public @interface Configurable {
     public String comment() default "";
 }
