a4593daa3025ee21eb227518a59587b018f55746
Itszuvalex <Itszuvalex@gmail.com>
2014-10-10 20:39:21 -0400

    Fix power and industry inheritance, use scala trait for power implementation.

--- src/main/java/com/itszuvalex/femtocraft/industry/tiles/TileEntityBaseEntityIndustry.java
+++ src/main/java/com/itszuvalex/femtocraft/industry/tiles/TileEntityBaseEntityIndustry.java
@@ -23,7 +23,7 @@ package com.itszuvalex.femtocraft.industry.tiles;
 
 import com.itszuvalex.femtocraft.power.tiles.TileEntityPowerConsumer;
 
-public class TileEntityBaseEntityIndustry extends TileEntityPowerConsumer {
+public abstract class TileEntityBaseEntityIndustry extends TileEntityPowerConsumer {
 
     @Override
     public void femtocraftServerUpdate() {
@@ -33,8 +33,7 @@ public class TileEntityBaseEntityIndustry extends TileEntityPowerConsumer {
             if (canStartWork()) {
                 startWork();
             }
-        }
-        else {
+        } else {
             continueWork();
         }
 
