3df1fc0c8acc9ffa2a22a4e60a098aab8e3891df
ccharri <ccharri@umich.edu>
2014-12-04 02:36:18 -0500

    Remove inheritance from obfuscated Minecraft class.

--- src/main/java/com/itszuvalex/femtocraft/sound/PositionedSoundLoopable.scala
+++ src/main/java/com/itszuvalex/femtocraft/sound/PositionedSoundLoopable.scala
@@ -1,19 +1,31 @@
 package com.itszuvalex.femtocraft.sound
 
+import cpw.mods.fml.relauncher.{Side, SideOnly}
+import net.minecraft.client.audio.ISound
 import net.minecraft.client.audio.ISound.AttenuationType
-import net.minecraft.client.audio.PositionedSound
 import net.minecraft.util.ResourceLocation
 
 /**
  * Created by Christopher Harris (Itszuvalex) on 10/22/14.
  */
-class PositionedSoundLoopable(location: ResourceLocation, vol: Float, pitch: Float, x: Int, y: Int, z: Int, repeats: Boolean, repeatDelay: Int, attenuation: AttenuationType) extends PositionedSound(location) {
-  this.volume = vol
-  this.field_147663_c = pitch
-  this.xPosF = x
-  this.yPosF = y
-  this.zPosF = z
-  this.repeat = repeats
-  this.field_147665_h = repeatDelay
-  this.field_147666_i = attenuation
+@SideOnly(Side.CLIENT)
+class PositionedSoundLoopable(val location: ResourceLocation, val vol: Float, val pitch: Float, val x: Int, val y: Int, val z: Int, val repeats: Boolean, val repeatDelay: Int, val attenuation: ISound.AttenuationType) extends ISound {
+  override def getPositionedSoundLocation: ResourceLocation = location
+
+  override def getRepeatDelay: Int = repeatDelay
+
+  override def getPitch: Float = pitch
+
+  override def getXPosF: Float = x
+
+  override def getVolume: Float = vol
+
+  override def getYPosF: Float = y
+
+  override def getAttenuationType: AttenuationType = attenuation
+
+  override def getZPosF: Float = z
+
+  override def canRepeat: Boolean = repeats
 }
+
