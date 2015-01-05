3c44e75379d5e1317db3ec22a8523583c2543f88
ccharri <ccharri@umich.edu>
2015-01-05 16:14:28 -0500

    Piston Press initial.
    DIdn't feel like triggering a whole new jenkins build for this.

---
+++ src/main/scala/com/itszuvalex/femtocraft/industry/multiblock/IPistonPressComponent.scala
@@ -0,0 +1,10 @@
+package com.itszuvalex.femtocraft.industry.multiblock
+
+import com.itszuvalex.femtocraft.api.multiblock.IMultiBlockComponent
+
+/**
+ * Created by Chris on 1/4/2015.
+ */
+trait IPistonPressComponent extends IMultiBlockComponent {
+
+}
