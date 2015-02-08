24cfa8e22159c37aac22423ae63fea4389a6f457
Itszuvalex <Itszuvalex@gmail.com>
2015-02-08 01:40:39 -0500

    DimensionalTear just starry background now

--- src/main/scala/com/itszuvalex/femtocraft/core/render/RenderDimensionalTear.scala
+++ src/main/scala/com/itszuvalex/femtocraft/core/render/RenderDimensionalTear.scala
@@ -19,13 +19,13 @@ import org.lwjgl.opengl.GL11
 }
 
 @SideOnly(Side.CLIENT) class RenderDimensionalTear extends TileEntitySpecialRenderer {
-  private var field_147528_b: FloatBuffer = GLAllocation.createDirectFloatBuffer(16)
+  private val field_147528_b: FloatBuffer = GLAllocation.createDirectFloatBuffer(16)
 
   def renderTileEntityAt(p_147500_1_ : TileEntity, p_147500_2_ : Double, p_147500_4_ : Double, p_147500_6_ : Double, p_147500_8_ : Float) {
     this.renderTileEntityAt(p_147500_1_.asInstanceOf[TileEntityDimensionalTear], p_147500_2_, p_147500_4_, p_147500_6_, p_147500_8_)
   }
 
-  def renderTileEntityAt(p_147500_1_ : TileEntityDimensionalTear, p_147500_2_ : Double, p_147500_4_ : Double, p_147500_6_ : Double, p_147500_8_ : Float) {
+  def renderTileEntityAt(tileEntity : TileEntityDimensionalTear, x : Double, y : Double, z : Double, p_147500_8_ : Float) {
     val f1: Float = this.field_147501_a.field_147560_j.toFloat
     val f2: Float = this.field_147501_a.field_147561_k.toFloat
     val f3: Float = this.field_147501_a.field_147558_l.toFloat
@@ -33,81 +33,68 @@ import org.lwjgl.opengl.GL11
     random.setSeed(31100L)
     val height: Float = 1
 
-    for (i <- 0 until 16) {
-
-      GL11.glPushMatrix()
-      var f5: Float = (16 - i).toFloat
-      var f6: Float = 0.0625F
-      var f7: Float = 1.0F / (f5 + 1.0F)
-      if (i == 0) {
-        this.bindTexture(skyLocation)
-        f7 = 0.1F
-        f5 = 65.0F
-        f6 = 0.125F
-        GL11.glEnable(GL11.GL_BLEND)
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
-      }
-      if (i == 1) {
-        this.bindTexture(pictureLocation)
-        GL11.glEnable(GL11.GL_BLEND)
-        GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE)
-        f6 = 0.5F
-      }
-      val f8: Float = (-(p_147500_4_ + height.toDouble)).toFloat
-      var f9: Float = f8 + ActiveRenderInfo.objectY
-      val f10: Float = f8 + f5 + ActiveRenderInfo.objectY
-      var f11: Float = f9 / f10
-      f11 += (p_147500_4_ + height.toDouble).toFloat
-      GL11.glTranslatef(f1, f11, f3)
-      GL11.glTexGeni(GL11.GL_S, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
-      GL11.glTexGeni(GL11.GL_T, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
-      GL11.glTexGeni(GL11.GL_R, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
-      GL11.glTexGeni(GL11.GL_Q, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_EYE_LINEAR)
-      GL11.glTexGen(GL11.GL_S, GL11.GL_OBJECT_PLANE, this.func_147525_a(1.0F, 0.0F, 0.0F, 0.0F))
-      GL11.glTexGen(GL11.GL_T, GL11.GL_OBJECT_PLANE, this.func_147525_a(0.0F, 0.0F, 1.0F, 0.0F))
-      GL11.glTexGen(GL11.GL_R, GL11.GL_OBJECT_PLANE, this.func_147525_a(0.0F, 0.0F, 0.0F, 1.0F))
-      GL11.glTexGen(GL11.GL_Q, GL11.GL_EYE_PLANE, this.func_147525_a(0.0F, 1.0F, 0.0F, 0.0F))
-      GL11.glEnable(GL11.GL_TEXTURE_GEN_S)
-      GL11.glEnable(GL11.GL_TEXTURE_GEN_T)
-      GL11.glEnable(GL11.GL_TEXTURE_GEN_R)
-      GL11.glEnable(GL11.GL_TEXTURE_GEN_Q)
-      GL11.glPopMatrix()
-      GL11.glMatrixMode(GL11.GL_TEXTURE)
-      GL11.glPushMatrix()
-      GL11.glLoadIdentity()
-      GL11.glTranslatef(0.0F, (Minecraft.getSystemTime % 700000L).toFloat / 700000.0F, 0.0F)
-      GL11.glScalef(f6, f6, f6)
-      GL11.glTranslatef(0.5F, 0.5F, 0.0F)
-      GL11.glRotatef((i * i * 4321 + i * 9).toFloat * 2.0F, 0.0F, 0.0F, 1.0F)
-      GL11.glTranslatef(-0.5F, -0.5F, 0.0F)
-      GL11.glTranslatef(-f1, -f3, -f2)
-      f9 = f8 + ActiveRenderInfo.objectY
-      GL11.glTranslatef(ActiveRenderInfo.objectX * f5 / f9, ActiveRenderInfo.objectZ * f5 / f9, -f2)
-      val tessellator = Tessellator.instance
-      tessellator.startDrawingQuads()
-      f11 = random.nextFloat * 0.5F + 0.1F
-      var f12 = random.nextFloat * 0.5F + 0.4F
-      var f13 = random.nextFloat * 0.5F + 0.5F
-      if (i == 0) {
-        f13 = 1.0F
-        f12 = 1.0F
-        f11 = 1.0F
-      }
-      tessellator.setColorRGBA_F(f11 * f7, f12 * f7, f13 * f7, 1.0F)
-      //render top
-      tessellator.addVertex(p_147500_2_, p_147500_4_ + height.toDouble, p_147500_6_)
-      tessellator.addVertex(p_147500_2_, p_147500_4_ + height.toDouble, p_147500_6_ + 1.0D)
-      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ + height.toDouble, p_147500_6_ + 1.0D)
-      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ + height.toDouble, p_147500_6_)
-      //render bot
-      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ , p_147500_6_)
-      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ , p_147500_6_ + 1.0D)
-      tessellator.addVertex(p_147500_2_, p_147500_4_ , p_147500_6_ + 1.0D)
-      tessellator.addVertex(p_147500_2_, p_147500_4_ , p_147500_6_)
-      tessellator.draw
-      GL11.glPopMatrix()
-      GL11.glMatrixMode(GL11.GL_MODELVIEW)
-    }
+    GL11.glPushMatrix()
+    var f5: Float = 16f
+    var f6: Float = 0.0625F
+    var f7: Float = 1.0F / (f5 + 1.0F)
+    this.bindTexture(skyLocation)
+    f7 = 0.1F
+    f5 = 65.0F
+    f6 = 0.125F
+    GL11.glEnable(GL11.GL_BLEND)
+    GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
+    val f8: Float = (-(y + height.toDouble)).toFloat
+    var f9: Float = f8 + ActiveRenderInfo.objectY
+    val f10: Float = f8 + f5 + ActiveRenderInfo.objectY
+    var f11: Float = f9 / f10
+    f11 += (y + height.toDouble).toFloat
+    GL11.glTranslatef(f1, f11, f3)
+    GL11.glTexGeni(GL11.GL_S, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
+    GL11.glTexGeni(GL11.GL_T, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
+    GL11.glTexGeni(GL11.GL_R, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_OBJECT_LINEAR)
+    GL11.glTexGeni(GL11.GL_Q, GL11.GL_TEXTURE_GEN_MODE, GL11.GL_EYE_LINEAR)
+    GL11.glTexGen(GL11.GL_S, GL11.GL_OBJECT_PLANE, this.func_147525_a(1.0F, 0.0F, 0.0F, 0.0F))
+    GL11.glTexGen(GL11.GL_T, GL11.GL_OBJECT_PLANE, this.func_147525_a(0.0F, 0.0F, 1.0F, 0.0F))
+    GL11.glTexGen(GL11.GL_R, GL11.GL_OBJECT_PLANE, this.func_147525_a(0.0F, 0.0F, 0.0F, 1.0F))
+    GL11.glTexGen(GL11.GL_Q, GL11.GL_EYE_PLANE, this.func_147525_a(0.0F, 1.0F, 0.0F, 0.0F))
+    GL11.glEnable(GL11.GL_TEXTURE_GEN_S)
+    GL11.glEnable(GL11.GL_TEXTURE_GEN_T)
+    GL11.glEnable(GL11.GL_TEXTURE_GEN_R)
+    GL11.glEnable(GL11.GL_TEXTURE_GEN_Q)
+    GL11.glPopMatrix()
+    GL11.glMatrixMode(GL11.GL_TEXTURE)
+    GL11.glPushMatrix()
+    GL11.glLoadIdentity()
+    GL11.glTranslatef(0.0F, (Minecraft.getSystemTime % 700000L).toFloat / 700000.0F, 0.0F)
+    GL11.glScalef(f6, f6, f6)
+    GL11.glTranslatef(0.5F, 0.5F, 0.0F)
+    GL11.glRotatef(4321F * 2.0F, 0.0F, 0.0F, 1.0F)
+    GL11.glTranslatef(-0.5F, -0.5F, 0.0F)
+    GL11.glTranslatef(-f1, -f3, -f2)
+    f9 = f8 + ActiveRenderInfo.objectY
+    GL11.glTranslatef(ActiveRenderInfo.objectX * f5 / f9, ActiveRenderInfo.objectZ * f5 / f9, -f2)
+    val tessellator = Tessellator.instance
+    tessellator.startDrawingQuads()
+    f11 = random.nextFloat * 0.5F + 0.1F
+    var f12 = random.nextFloat * 0.5F + 0.4F
+    var f13 = random.nextFloat * 0.5F + 0.5F
+    f13 = 1.0F
+    f12 = 1.0F
+    f11 = 1.0F
+    tessellator.setColorRGBA_F(f11 * f7, f12 * f7, f13 * f7, 1.0F)
+    //render top
+    tessellator.addVertex(x, y + height.toDouble, z)
+    tessellator.addVertex(x, y + height.toDouble, z + 1.0D)
+    tessellator.addVertex(x + 1.0D, y + height.toDouble, z + 1.0D)
+    tessellator.addVertex(x + 1.0D, y + height.toDouble, z)
+    //render bot
+    //      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ , p_147500_6_)
+    //      tessellator.addVertex(p_147500_2_ + 1.0D, p_147500_4_ , p_147500_6_ + 1.0D)
+    //      tessellator.addVertex(p_147500_2_, p_147500_4_ , p_147500_6_ + 1.0D)
+    //      tessellator.addVertex(p_147500_2_, p_147500_4_ , p_147500_6_)
+    tessellator.draw()
+    GL11.glPopMatrix()
+    GL11.glMatrixMode(GL11.GL_MODELVIEW)
     GL11.glDisable(GL11.GL_BLEND)
     GL11.glDisable(GL11.GL_TEXTURE_GEN_S)
     GL11.glDisable(GL11.GL_TEXTURE_GEN_T)
